d628bea221e256085b6acd9ef363fd6198c2f0af
Michal Hoffman <michal.z.hoffman@gmail.com>
2023-02-16 15:06:24 +0000

    moving inputs logic to a custom hook

---
+++ src/hooks/use-inputs.js
@@ -0,0 +1,10 @@
+import { useState } from 'react'
+
+const useInputs = () => {
+  const [inputValue, setInputValue] = useState('')
+  const [isTouched, setIsTouched] = useState(false)
+
+  return {}
+}
+
+export default useInputs
